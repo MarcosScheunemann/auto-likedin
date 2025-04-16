e1561f702444a73f17c4a80df9e333640d72b726
Marcos <vergueiro81@gmail.com>
2025-04-16 08:46:15 -0300

    refactor: enhance error handling and improve LinkedIn post generation in GptService

--- src/app/ia-services/gpt/gpt.service.ts
+++ src/app/ia-services/gpt/gpt.service.ts
@@ -1,10 +1,24 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
 import axios from 'axios';
-import { ChatGptResponseEntity, EChatGptModel, EChatGptRole } from 'scheunemann-interfaces';
+import {
+  ChatGptResponseEntity,
+  EChatGptModel,
+  EChatGptRole,
+} from 'scheunemann-interfaces';
 
 @Injectable()
 export class GptService {
-  constructor() {}
+  private gptUrl: string;
+  private gptKey?: string;
+  constructor() {
+    this.gptUrl = 'https://api.openai.com/v1/chat/completions';
+    this.gptKey = process.env.OPENAI_API_KEY;
+  }
   public async makeResume(news: string[] | null): Promise<string> {
     if (!news) {
       throw new BadRequestException('Sem noticias para tratar!');
@@ -25,28 +39,68 @@ export class GptService {
     }[] = [
       {
         role: EChatGptRole.SYSTEM,
-        content: `Você é um especialista em tecnologia e jornalismo. Sua tarefa é analisar um conjunto de notícias sobre tecnologia 
-        e selecionar a mais relevante, considerando impacto na sociedade, inovação e relevância global. Após escolher, 
-        você deve fornecer um resumo detalhado da notícia, mantendo os pontos mais importantes.`,
+        content: `You are a technology and journalism expert. Your task is to analyze a set of technology news articles and select the most relevant one, considering societal impact, innovation, and global relevance. After choosing, you must provide a detailed summary of the news, keeping the most important points.`,
       },
       ...notes,
       {
         name: 'Marcos',
         role: EChatGptRole.USER,
         content:
-          'Agora, escolha a notícia mais relevante e forneça um resumo detalhado.',
+          'Now, choose the most relevant news and provide a detailed summary.',
       },
     ];
-    const response = await axios.post<ChatGptResponseEntity>(
-      'https://api.openai.com/v1/chat/completions',
-      { messages, model: EChatGptModel.GPT_4O },
-      {
-        headers: {
-          Authorization: process.env.OPENAI_API_KEY,
-          'Content-Type': 'application/json',
+    try {
+      const response = await axios.post<ChatGptResponseEntity>(
+        this.gptUrl,
+        { messages, model: EChatGptModel.GPT_4O },
+        {
+          headers: {
+            Authorization: this.gptKey,
+            'Content-Type': 'application/json',
+          },
         },
+      );
+      return response.data.choices[0].message.content;
+    } catch (error: any) {
+      throw new HttpException(
+        error.response?.data || error.message,
+        error.response?.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+  public async makePostText(text: string): Promise<string> {
+    if (!text) {
+      throw new BadRequestException('Sem texto para tratar!');
+    }
+    const messages = [
+      {
+        role: EChatGptRole.SYSTEM,
+        content: `You are an experienced 5 years developer NestJs Backend. Your task is to create a LinkedIn post about technology based on the text the "user" sends. 
+        The post should be engaging, put human feelings subtly, add some of your own experience to the post only if it makes sense with the news. 
+        Avoid using emojis. The post must have a maximum of 1500 characters and be written in Portuguese. ATENTION! the post cannot contain special characters like )({}@ ,ALWAYS add "Segue pra mais!" at the end of the post.`,
       },
-    );
-    return response.data.choices[0].message.content;
+      {
+        role: EChatGptRole.USER,
+        content: text,
+      },
+    ];
+    try {
+      const response = await axios.post<ChatGptResponseEntity>(
+        this.gptUrl,
+        { messages, model: EChatGptModel.GPT_4O },
+        {
+          headers: {
+            Authorization: this.gptKey,
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      return response.data.choices[0].message.content;
+    } catch (error: any) {
+      throw new HttpException(
+        error.response?.data || error.message,
+        error.response?.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
